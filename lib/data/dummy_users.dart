97bc072c84a6ef311dbffc6f4f8ec873d3c57944
Lucas Ferreira <lucasferreira@iMac-de-Lucas.local>
2020-12-22 12:18:38 -0300

    importacao inicial

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Lucas',
+      email: 'lucasdpcferreira@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Dani',
+      email: 'daniela.carreiro129@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Jorge',
+      email: 'jorgemanuelcf@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Flavia',
+      email: 'flavia@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png'),
+};
